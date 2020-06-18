1f726c1ba67186673addc465ddb511ed5e90defc
Mauricio <mauricioalexandre.com@outlook.com>
2020-06-17 23:29:33 -0300

    operacao repository - Ok

--- Application.Tests/Persistence/OperacaoRepositoryTest.cs
+++ Application.Tests/Persistence/OperacaoRepositoryTest.cs
@@ -1,14 +1,47 @@
-﻿using NUnit.Framework;
+﻿using System;
+using Application.Tests.Utils;
+using Lab.ExchangeNet45.Application.Operacao.CommandSide.DomainModels;
+using Lab.ExchangeNet45.Application.Operacao.CommandSide.Persistence;
+using NUnit.Framework;
 
 namespace Application.Tests.Persistence
 {
     [TestFixture]
     public class OperacaoRepositoryTest : NHibernateSqLiteTestFixture
     {
-        [Test]
-        public void ShouldPass()
+        private void Clear()
         {
-            Assert.True(true);
+            ExecuteSessionTransactionScope(session =>
+            {
+                session.CreateQuery($"delete from {nameof(Operacao)}").ExecuteUpdate();
+            });
+        }
+
+        [SetUp]
+        public void Before() => Clear();
+
+        [TearDown]
+        public void After() => Clear();
+
+        [Test]
+        public void When_ANewOperacaoIsAdded_Expect_ToBeFoundOnTheDatabase()
+        {
+            Operacao aNewOperacao = null;
+
+            ExecuteSessionTransactionScope(session =>
+            {
+                aNewOperacao = new Operacao(1, new DateTime(2020, 6, 17, 10, 33, 45), OperacaoTipo.Compra, "VIVT4", 100, 49.70d, 9999);
+                new OperacaoRepository(session).Add(aNewOperacao);
+            });
+
+            Operacao foundOnTheDatabase = null;
+
+            ExecuteSessionTransactionScope(session =>
+            {
+                foundOnTheDatabase = new OperacaoRepository(session).FindById(aNewOperacao.Id);
+            });
+
+            Assert.True(aNewOperacao.PublicInstancePropertiesEqual(foundOnTheDatabase));
         }
     }
 }
---
+++ Application.Tests/Utils/ObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Application.Tests.Utils
+{
+    internal static class ObjectExtensions
+    {
+        public static bool PublicInstancePropertiesEqual<T>(this T self, T to, params string[] ignore) where T : class
+        {
+            if (self == null || to == null) return self == to;
+
+            Type type = typeof(T);
+            IEnumerable<string> ignoreList = new List<string>(ignore);
+
+            IEnumerable<object> unequalProperties =
+                from pi in type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                where !ignoreList.Contains(pi.Name) && pi.GetUnderlyingType().IsSimpleType() && pi.GetIndexParameters().Length == 0
+                let selfValue = type.GetProperty(pi.Name)?.GetValue(self, null)
+                let toValue = type.GetProperty(pi.Name)?.GetValue(to, null)
+                where selfValue != toValue && (selfValue == null || !selfValue.Equals(toValue))
+                select selfValue;
+
+            return !unequalProperties.Any();
+        }
+    }
+}
