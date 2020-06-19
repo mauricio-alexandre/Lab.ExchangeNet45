d19a19122a678167b47308788cf2876d74c8cfcc
Mauricio <mauricioalexandre.com@outlook.com>
2020-06-18 21:20:52 -0300

    adding Loader project - "carregador de massa de dados"

---
+++ OperacaoLoader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab.ExchangeNet45.OperacaoLoader
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Title = "Exchange Operação Loader - Carregador de Massa de Dados";
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
